38ab444a30c573d188a6e2486b3efc30b54a12ed
Tuomas Artman <tuomas@artman.fi>
2014-09-01 16:13:59 -0700

    Initial Commit

---
+++ HexColor/HexColor.h
@@ -0,0 +1,19 @@
+//
+//  HexColor.h
+//  HexColor
+//
+//  Created by Tuomas Artman on 1.9.2014.
+//  Copyright (c) 2014 Tuomas Artman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HexColor.
+FOUNDATION_EXPORT double HexColorVersionNumber;
+
+//! Project version string for HexColor.
+FOUNDATION_EXPORT const unsigned char HexColorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HexColor/PublicHeader.h>
+
+
